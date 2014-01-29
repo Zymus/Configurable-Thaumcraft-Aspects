104ccaa971a09e699fae7e24e95b6ba487090393
Zyle Moore <moore.zyle@gmail.com>
2014-01-29 14:20:32 -0800

    Add ConfigurationAdapter class

---
+++ src/org/zephyrion/cta/ConfigurationAdapter.java
@@ -0,0 +1,82 @@
+package org.zephyrion.cta;
+
+import java.util.Set;
+
+import net.minecraftforge.common.ConfigCategory;
+import net.minecraftforge.common.Configuration;
+
+/**
+ * This class adapts a Minecraft Forge Configuration object to more simply 
+ * access only the functions necessary for the mod.
+ * 
+ * @author Zymus
+ * @version 0.1
+ * @since 0.1
+ */
+public final class ConfigurationAdapter {
+
+    /**
+     * Constructs a ConfigurationAdapter with the specified Minecraft Forge 
+     * config.
+     * 
+     * @param config the Minecraft Forge config
+     */
+    public ConfigurationAdapter(final Configuration config) {
+        if (config == null) {
+            throw new IllegalArgumentException("config must not be null");
+        }
+        this.config = config;
+        this.categoryNames = config.getCategoryNames();
+    }
+
+    /**
+     * Returns the Minecraft Forge Configuration that was passed in to the 
+     * constructor.
+     * 
+     * @return the Minecraft Forge Configuration
+     */
+    public Configuration getConfig() {
+        return config;
+    }
+
+    /**
+     * Returns whether or not the Minecraft Forge Configuration contains the 
+     * specified key.
+     * 
+     * @param key the key name
+     * @return true if the key is found; false otherwise
+     */
+    public boolean containsKey(final String key) {
+        for(final String categoryName : categoryNames) {
+            final ConfigCategory category = config.getCategory(categoryName);
+            if (config.hasKey(categoryName, key)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Returns the value from the Minecraft Forge Configuration with the 
+     * specified key.
+     * 
+     * @param key the key name
+     * @return the value of the key
+     */
+    public int getValue(final String key) {
+        if (key == null) {
+            throw new IllegalArgumentException("key must not be null");
+        }
+        for(final String categoryName : categoryNames) {
+            if (config.hasKey(categoryName, key)) {
+                return config.get(categoryName, key, -1).getInt();
+            }
+        }
+        throw new UnsupportedOperationException(
+            "no key found with name \"" + key + "\"");
+    }
+    
+    
+    private final Configuration config;
+    private final Set<String> categoryNames;
+}
